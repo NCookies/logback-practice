2584e92b222df9419c6c974000cec4d9dde9d9f7
SeungWoo Ryu <swstar21c@gmail.com>
2024-12-08 18:38:33 +0900

    first commit

---
+++ src/main/java/com/ncookie/logbackpractice/LogbackPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ncookie.logbackpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncookie/logbackpractice/LogbackPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncookie.logbackpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
